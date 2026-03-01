95f3617cf00cec11e0553c119d6c94ad16126bc4
ankit6370232757 <ankitkumarrath5@gmail.com>
2026-03-01 14:11:28 +0530

    daily payout

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,4 +14,5 @@ const PORT = process.env.PORT || 5000;
 // Start server
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
+    require("./services/income.service").runDailyIncome();
 });
