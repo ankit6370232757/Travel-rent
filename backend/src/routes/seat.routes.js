a22b1066856027c2c7c1d7dd8f2ef2778acfe3db
Nilamani Dhal <dhalnilamani398@gmail.com>
2026-03-17 15:14:19 +0530

    all all package state opti

--- backend/src/routes/seat.routes.js
+++ backend/src/routes/seat.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const seatController = require("../controllers/seat.controller");
 
+router.get("/all-status", seatController.getAllPackagesWithStatus);
 router.get("/status/:packageName", seatController.getSeatStatus);
 
 module.exports = router;
